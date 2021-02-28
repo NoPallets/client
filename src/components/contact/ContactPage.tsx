5bd9d565407eaf72d1defa34805bdd3c03c12cb0
StanislavSt <s.staev1@gmail.com>
2021-02-28 21:27:28 +0100

    add contact page

---
+++ src/components/contact/ContactPage.tsx
@@ -0,0 +1,11 @@
+import Layout from "../layout/Layout";
+
+const ContactPage = () => {
+  return (
+    <Layout title="contact">
+      <div>Hello from contact</div>
+    </Layout>
+  );
+};
+
+export default ContactPage;

07cfe83f64fb89b60ce4f72e8c5a086e9d3a6b0e
StanislavSt <s.staev1@gmail.com>
2021-02-28 21:27:03 +0100

    add about page

---
+++ src/components/about/AboutPage.tsx
@@ -0,0 +1,11 @@
+import Layout from "../layout/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout title="About">
+      <div>hello from About</div>
+    </Layout>
+  );
+};
+
+export default AboutPage;

c4d853b9d00443a245861d412c1e1aaf7616ec78
StanislavSt <s.staev1@gmail.com>
2021-02-28 21:27:33 +0100

    add links

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 const Nav = () => {
   return (
     <header className="w-auto xl:w-4/5 mx-auto sticky z-10">
       <div className="flex justify-between items-center">
-        <span className="cursor-pointer hover:opacity-50">About NP</span>
-        <a className="cursor-pointer text-6xl text-bold ">NoPallets</a>
-        <a className="cursor-pointer hover:opacity-50">Contact NP</a>
+        <Link href="/about">
+          <a className="cursor-pointer hover:opacity-50">About NP</a>
+        </Link>
+        <Link href="/">
+          <a className="cursor-pointer text-6xl text-bold ">NoPallets</a>
+        </Link>
+        <Link href="/contact">
+          <a className="cursor-pointer hover:opacity-50">Contact NP</a>
+        </Link>
       </div>
     </header>
   );

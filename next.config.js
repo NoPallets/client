ff0b7d62bbd3c73855abf49aa1c91cb3bba9318a
StanislavSt <s.staev1@gmail.com>
2021-02-28 19:38:33 +0100

    add tailwindcss

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
-module.exports = {
+const withFonts = require("next-fonts");
+
+module.exports = withFonts({
   images: {
     domains: ["nopallets-product-image.s3.eu-central-1.amazonaws.com"],
   },
-};
+  webpack(config, options) {
+    config.node = {
+      fs: "empty",
+    };
+    config.module.rules.push({
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "url-loader?limit=100000",
+        },
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
+    return config;
+  },
+});

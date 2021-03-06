e48914e394509656c48d997412bae40d7162a0c5
StanislavSt <s.staev1@gmail.com>
2021-03-07 00:04:28 +0100

    fix footer position

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-5 w-full flex justify-center z-10 text-sm font-bold">
+    <footer className="fixed bottom-5 -left-1 w-full flex justify-center z-10 text-sm font-bold">
       <div className="">
         <span className="mx-2">Copyright © {new Date().getFullYear()}.</span>
         <span>NoPallets</span>

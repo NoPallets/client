e962186cd3138092b4944725c4cc3fc0e1e14786
StanislavSt <s.staev1@gmail.com>
2021-03-06 23:50:46 +0100

    style:postcard hover

--- src/components/index/postcard/PostCard.tsx
+++ src/components/index/postcard/PostCard.tsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import { Product } from "../../../graphql/generated/graphql";
+import style from "./PostCard.module.css";
 
 interface Props {
   product: Product;
 }
 
 const PostCard = ({ product }: Props) => {
-  return <Image src={product.s3_url} width={450} height={450} />;
+  return (
+    <div className={` ${style.postCard} relative`}>
+      <span
+        className={`${style.title} absolute z-10 top-1/2 left-1/2 transform -translate-y-2/4 -translate-x-2/4`}
+      >
+        {product.title}
+      </span>
+      <Image
+        src={product.s3_url}
+        width={450}
+        height={450}
+        className="hover:opacity-30 cursor-pointer"
+        objectFit="cover"
+      />
+    </div>
+  );
 };
 
 export default PostCard;
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="fixed bottom-5 w-full flex justify-center z-10 text-sm font-bold">
-      <div className="pr-14">
+      <div className="">
         <span className="mx-2">Copyright © {new Date().getFullYear()}.</span>
         <span>NoPallets</span>
       </div>

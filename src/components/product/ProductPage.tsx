b754a2b529c6bd02f8ebd6d221226f398d5be559
StanislavSt <s.staev1@gmail.com>
2021-04-26 22:34:55 +0200

    feat: add swiper.js

--- src/components/product/ProductPage.tsx
+++ src/components/product/ProductPage.tsx
@@ -1,6 +1,12 @@
 import Layout from "../layout/Layout";
-import { useRouter } from "next/router";
 import { Products } from "../../graphql/generated/graphql";
+import Image from "next/image";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import SwiperCore, { EffectFade, Navigation } from "swiper";
+SwiperCore.use([EffectFade, Navigation]);
 
 interface Props {
   product: Products;
@@ -10,6 +16,23 @@ const ProductPage = ({ product }: Props) => {
   return (
     <Layout>
       <p>Post:{product.title}</p>
+      <Swiper
+        autoHeight
+        style={{ width: "750px" }}
+        spaceBetween={50}
+        slidesPerView={1}
+        loop
+        navigation
+        initialSlide={0}
+        effect="fade"
+      >
+        {product.images.map((img) => (
+          <SwiperSlide>
+            <Image src={img} width={900} height={1250} objectFit="cover" />
+          </SwiperSlide>
+        ))}
+        ...
+      </Swiper>
     </Layout>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
 
+import "swiper/swiper.scss";
 import "../styles/globals.css";
+import "swiper/components/effect-fade/effect-fade.scss";
+import 'swiper/components/navigation/navigation.scss';
+
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);

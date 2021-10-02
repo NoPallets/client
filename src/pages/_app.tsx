055904e3921b4ffd22f8a67bb21125e7f915f22e
StanislavSt <s.staev1@gmail.com>
2021-10-02 16:33:39 +0300

    chore(_app): double quotes instead of single

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,7 @@ import { useApollo } from "../lib/apolloClient";
 import "swiper/swiper.scss";
 import "../styles/globals.css";
 import "swiper/components/effect-fade/effect-fade.scss";
-import 'swiper/components/navigation/navigation.scss';
-
+import "swiper/components/navigation/navigation.scss";
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);

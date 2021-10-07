34c998850e36a62b1e04377ccb8b131055ec91a5
StanislavSt <s.staev1@gmail.com>
2021-10-07 22:32:51 +0200

    feat(helpers): add parseJWT with no dependencies

---
+++ src/lib/helpers.ts
@@ -0,0 +1,21 @@
+type ParsedToken = {
+  email: String;
+  exp: Number;
+  iat: Number;
+  sub: String;
+};
+
+export const parseJwt = (token): ParsedToken => {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+  console.log("parsing this shit", JSON.parse(jsonPayload));
+  return JSON.parse(jsonPayload);
+};

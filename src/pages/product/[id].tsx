fe7b1fddc11abaca1dab3babb14023641f1ed4ed
StanislavSt <s.staev1@gmail.com>
2021-10-28 20:07:39 +0200

    use SSR for dynamic posts

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,49 +1,25 @@
 import ProductPage from "../../components/product/ProductPage";
 import { initializeApollo } from "../../lib/apolloClient";
-import { GetProduct, GetProducts } from "../../graphql/queries";
+import { GetProduct } from "../../graphql/queries";
 import {
   GetProductQuery,
   GetProductQueryVariables,
-  GetProductsQuery,
 } from "../../graphql/generated/graphql";
-import { GetStaticProps, GetStaticPaths } from "next";
-
-import { useQuery } from "@apollo/client";
+import { GetServerSideProps } from "next";
 
 export default ProductPage;
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  try {
-    const apolloClient = initializeApollo();
-
-    const { data } = await apolloClient.query<
-      GetProductQuery,
-      GetProductQueryVariables
-    >({ query: GetProduct, variables: { id: params.id } });
-
-    return {
-      props: {
-        product: data.products_by_pk,
-      },
-    };
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const apolloClient = initializeApollo();
 
-  const { data } = await apolloClient.query<GetProductsQuery>({
-    query: GetProducts,
-  });
-
-  const paths = data.products.map(({ id }) => ({
-    params: { id },
-  }));
+  const { data } = await apolloClient.query<
+    GetProductQuery,
+    GetProductQueryVariables
+  >({ query: GetProduct, variables: { id: params.id } });
 
   return {
-    paths: [],
-    fallback: true,
+    props: {
+      product: data.products_by_pk,
+    },
   };
 };
